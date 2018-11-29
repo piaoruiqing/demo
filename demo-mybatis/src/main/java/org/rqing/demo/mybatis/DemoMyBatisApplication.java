173b749639c73f7ec260b788333bfb6a8f227dce
piaoruiqing <piaoruiqing@hotmail.com>
2018-11-29 09:13:08 +0800

    [MOD] page helper demo

--- demo-mybatis/src/main/java/org/rqing/demo/mybatis/DemoMyBatisApplication.java
+++ demo-mybatis/src/main/java/org/rqing/demo/mybatis/DemoMyBatisApplication.java
@@ -15,12 +15,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.github.pagehelper.ISelect;
 import com.github.pagehelper.Page;
 import com.github.pagehelper.PageHelper;
 import com.github.pagehelper.PageInfo;
 
-@EnableAutoConfiguration
 @SpringBootApplication
+@EnableAutoConfiguration
 @RestController
 @RequestMapping("/demo/mybatis")
 public class DemoMyBatisApplication {
@@ -28,19 +29,42 @@ public class DemoMyBatisApplication {
     @Autowired
     private UserMapper userMapper;
     
-    @RequestMapping(value = "/page")
-    public ResponseEntity<?> page(
+    @RequestMapping(value = "/page/lambda")
+    public ResponseEntity<?> lambda(
             @RequestParam(value = "limit", defaultValue = "10") Integer limit,
             @RequestParam(value = "offset", defaultValue = "0") Integer offset){
+        
+        Page<User> page = PageHelper.offsetPage(offset, limit).doSelectPage(() -> userMapper.page(0L, Long.MAX_VALUE));
+        return ResponseEntity.ok(ResponseData.success(new PageInfo<>(page)));
+    }
+    
+    @RequestMapping(value = "/page/static_page_helper")
+    public ResponseEntity<?> staticPageHelper(
+        @RequestParam(value = "limit", defaultValue = "10") Integer limit,
+        @RequestParam(value = "offset", defaultValue = "0") Integer offset){
         
         PageHelper.offsetPage(offset, limit);
         Page<User> page = userMapper.page(0L, Long.MAX_VALUE);
         return ResponseEntity.ok(ResponseData.success(new PageInfo<>(page)));
     }
     
+    @RequestMapping(value = "/page/iselect")
+    public ResponseEntity<?> iselect(
+        @RequestParam(value = "limit", defaultValue = "10") Integer limit,
+        @RequestParam(value = "offset", defaultValue = "0") Integer offset){
+        
+        Page<User> page = PageHelper.offsetPage(offset, limit).doSelectPage(new ISelect() {
+            @Override
+            public void doSelect() {
+                userMapper.page(0L, Long.MAX_VALUE);
+            }
+        });
+        return ResponseEntity.ok(ResponseData.success(new PageInfo<>(page)));
+    }
+    
     @RequestMapping(value = "/add")
     public ResponseEntity<?> add(){
-        User user = User.builder().name(UUID.randomUUID().toString()).age(new Random().nextInt()).build();
+        User user = User.builder().name(UUID.randomUUID().toString()).age(new Random().nextInt(100)).build();
         userMapper.insert(user);
         return ResponseEntity.ok(user);
     }
